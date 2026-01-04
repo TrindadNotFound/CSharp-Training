c803a1dfa7c0b96dd59355e9725a10e16b1239c9
TrindadNotFound <ricardo-x-trindade@hotmail.com>
2026-01-04 02:15:34 +0000

    Section 6 - List exercise (Registe employees)

---
+++ S6-RegisteEmployees/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Write("How many employees will be registered ? ");
+int numberOfEmployee = int.Parse(Console.ReadLine());
+
+List<Employee> employeesList = new List<Employee>();
+
+for (int i = 1; i <= numberOfEmployee; i++)
+{
+    Console.WriteLine($"Employee #{i}");
+
+    Console.Write("ID : ");
+    int id = int.Parse(Console.ReadLine());
+
+    Console.Write("Name : ");
+    string name = Console.ReadLine();
+
+    Console.Write("Salary : ");
+    double salary = double.Parse(Console.ReadLine());
+
+    employeesList.Add(new Employee(id, name, salary));
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+Console.Write("Enter the employee ID that will have salary increase : ");
+int employeeIdToIncrease = int.Parse(Console.ReadLine());
+
+Employee employee = employeesList.Find(x => x.Id == employeeIdToIncrease);
+
+if (employee != null)
+{
+    Console.Write("Enter the percentage : ");
+    double increasePercentage = double.Parse(Console.ReadLine());
+
+    employee.IncreaseSalary(increasePercentage);
+}
+else
+{
+    Console.WriteLine("Employee ID dosen't exist");
+}
+
+Console.WriteLine();
+Console.WriteLine("Updated employee list :");
+foreach (Employee item in employeesList)
+{
+    Console.WriteLine(item);
+}
