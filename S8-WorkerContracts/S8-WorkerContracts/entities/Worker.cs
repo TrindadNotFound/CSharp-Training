350f8d854767f8b0d42449b99d2d32bed1b37b0e
TrindadNotFound <ricardo-x-trindade@hotmail.com>
2026-01-05 22:40:11 +0000

    AddContract, RemoveContract and Income methods implementation

--- S8-WorkerContracts/S8-WorkerContracts/entities/Worker.cs
+++ S8-WorkerContracts/S8-WorkerContracts/entities/Worker.cs
@@ -7,14 +7,42 @@ public class Worker
     public string Name { get; set; }
     public WorkerLevel Level { get; set; }
     public double BaseSalary { get; set; }
+    public Department Department { get; set; }
+    public List<HourContract> Contract { get; set; } = new List<HourContract>();
 
-    public Worker(string name, WorkerLevel workerLevel, double baseSalary)
+    public Worker() {}
+    public Worker(string name, WorkerLevel workerLevel, double baseSalary,  Department department)
     {
         Name = name;
         Level = workerLevel;
         BaseSalary = baseSalary;
+        Department = department;
     }
-    
+
+    public void AddContract(HourContract contract)
+    {
+        Contract.Add(contract);
+    }
+
+    public void RemoveContract(HourContract contract)
+    {
+        Contract.Remove(contract);
+    }
+
+    public double Income(int year, int month)
+    {
+        double sum = BaseSalary;
+
+        foreach (HourContract i in Contract)
+        {
+            if (i.Date.Year == year && i.Date.Month == month)
+            {
+                sum += i.totalValue();
+            }
+        }
+
+        return sum;
+    }
     public override string ToString()
     {
         return "xpto";
--- S8-WorkerContracts/S8-WorkerContracts/obj/Debug/net10.0/S8-WorkerContracts.AssemblyInfo.cs
+++ S8-WorkerContracts/S8-WorkerContracts/obj/Debug/net10.0/S8-WorkerContracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S8-WorkerContracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac7464df69bba4e11f1283ec66feba122c2bd40e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f77ed5619cfabf56110bc0d73bc9e2b615e69c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("S8-WorkerContracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S8-WorkerContracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
