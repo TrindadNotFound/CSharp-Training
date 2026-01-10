4da65913b20113674e320949c91e6ef78f8d95c3
TrindadNotFound <ricardo-x-trindade@hotmail.com>
2026-01-10 23:34:55 +0000

    Section 11 - FileStream and StreamReader example

---
+++ S11-StreamReaderExample/S11-StreamReaderExample/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string filePath = @"/Users/ricardotrindade/Documents/file1.txt";
+
+FileStream fileStream = null;
+StreamReader streamReader = null;
+
+try
+{
+    fileStream = new FileStream(filePath, FileMode.Open);
+    streamReader = new StreamReader(fileStream);
+    
+    string line = streamReader.ReadLine();
+
+    Console.WriteLine(line);
+    
+
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+} 
+finally
+{
+    fileStream.Close();
+    fileStream.Dispose();
+    
+    streamReader.Close();
+    streamReader.Dispose();
+}
