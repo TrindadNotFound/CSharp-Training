177be1da7a5e7fc92422c02d8a9aa4d1f8c2ac1c
TrindadNotFound <ricardo-x-trindade@hotmail.com>
2026-01-10 23:47:47 +0000

    Section 11 - Example with "using" and "StreamWriter"

---
+++ S11-ExampleWithUsing/S11-ExampleWithUsing/Program.cs
@@ -0,0 +1,30 @@
+﻿
+string sourcePath = @"/Users/ricardotrindade/Documents/file1.txt";
+string targetPath = @"/Users/ricardotrindade/Documents/file2.txt";
+
+try
+{
+    string[] lines = File.ReadAllLines(sourcePath); 
+    using (StreamReader streamReader = File.OpenText(sourcePath))
+    {
+        while (!streamReader.EndOfStream)
+        {
+            Console.WriteLine(streamReader.ReadLine());
+        }
+        
+        
+    }
+
+    using (StreamWriter streamWriter = File.AppendText(targetPath))
+    {
+        foreach (string st in lines)
+        {
+            streamWriter.WriteLine(st.ToUpper());
+        }
+    }
+    
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
