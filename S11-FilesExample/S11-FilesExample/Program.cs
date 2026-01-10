ec25e7e0cc4c65bb1de7e85f5e366a03fbc619e1
TrindadNotFound <ricardo-x-trindade@hotmail.com>
2026-01-10 19:09:37 +0000

    Section 11 - Simple example with FileInfo object

---
+++ S11-FilesExample/S11-FilesExample/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string sourcePath = @"/Users/ricardotrindade/Documents/file1.txt";
+string destinationPath = @"/Users/ricardotrindade/Documents/file2.txt";
+
+try
+{
+    FileInfo fileInfo = new FileInfo(sourcePath);
+    fileInfo.CopyTo(destinationPath);
+    Console.WriteLine("File {0} copied to {1}", sourcePath, destinationPath);
+    
+    string[] fileLines  = File.ReadAllLines(sourcePath); //Read all line from archive
+    
+    foreach (string line in fileLines)
+    {
+        Console.WriteLine(line);
+    }
+    
+    
+}
+catch (IOException e)
+{
+    Console.WriteLine($"Error : {e.Message}");
+}
