4a93c6e60cd284c080a983b4129b8d25faf539ae
TrindadNotFound <ricardo-x-trindade@hotmail.com>
2026-01-11 17:55:19 +0000

    Sectio 11 - List directory and files example

---
+++ S11-DirectoryInfo/S11-DirectoryInfo/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string sourcePath = @"/Users/ricardotrindade/Documents";
+
+try
+{
+   var folders = Directory.EnumerateDirectories(sourcePath, "*.*", SearchOption.TopDirectoryOnly);
+   Console.WriteLine("Folders :");
+   foreach (string i in folders)
+   {
+       Console.WriteLine(i);    
+   }
+   
+   Console.WriteLine();
+   
+   
+   var files = Directory.EnumerateFiles(sourcePath, "*.*", SearchOption.TopDirectoryOnly);
+   Console.WriteLine("Files :");
+   foreach (string i in files)
+   {
+       Console.WriteLine(i);
+   }
+
+   Directory.CreateDirectory(@"/Users/ricardotrindade/Documents/newFolder");
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
