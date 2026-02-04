81cdf120eacb31ef29780631c74e8a67ba6afe37
TrindadNotFound <ricardo-x-trindade@hotmail.com>
2026-02-04 00:55:51 +0000

    Section 13 - Example with dictionary

---
+++ S13-Dictionary/S13-Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+cookies["name"] = "Jane";
+cookies["age"] = "25";
+cookies["birthday"] = "2021-05-25";
+cookies["email"] = "jane@mail.com";
+
+Console.WriteLine(cookies["name"]);
+
+if (cookies.ContainsKey("email"))
+{
+    Console.WriteLine(cookies["email"]);
+}
+
+Console.WriteLine("--------------");
+
+foreach (KeyValuePair<string, string> item in cookies)
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
+}
