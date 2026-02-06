3151cfe9025215aeb7c69c9915c476322c23ccfd
R. Trindade <75436524+TrindadNotFound@users.noreply.github.com>
2026-02-06 15:38:00 +0000

    Add files via upload

---
+++ S15-Func/S15-Func/Program.cs
@@ -0,0 +1,27 @@
+﻿using S15_Func.Entities;
+using System.Linq;
+namespace S15_Func;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Product> list = new List<Product>();
+        
+        list.Add(new Product("TV", 900));
+        list.Add(new Product("XBox", 600));
+        list.Add(new Product("Samsung Galaxy", 600));
+        list.Add(new Product("iPhone X", 600));
+        list.Add(new Product("iPhone X Pro", 600));
+
+
+        Func<Product, string> func = p => p.Name.ToUpper();
+        
+        List<string> result = list.Select(func).ToList();
+
+        foreach (String s in result)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
