cc78db8e3a231e06a7a5d3a353236172de9904b7
R. Trindade <75436524+TrindadNotFound@users.noreply.github.com>
2026-02-06 15:34:36 +0000

    Add files via upload

---
+++ S15-Delegate/S15-Delegate/Program.cs
@@ -0,0 +1,24 @@
+﻿using S15_Delegate.Service;
+
+namespace S15_Delegate;
+
+//delegate double BinaryNumericOperation(double n1, double n2);
+delegate void BinaryNumericOperation(double n1, double n2);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a = 10;
+        double b = 20;
+
+        //BinaryNumericOperation bno = CalculationService.Sum;
+        //double result = bno(a, b);
+        
+        
+        BinaryNumericOperation bno = CalculationService.ShowSum;
+        bno += CalculationService.ShowMax;
+        
+        bno(a,b);
+    }
+}
---
+++ S15-Delegate/S15-Delegate/Service/CalculationService.cs
@@ -0,0 +1,31 @@
+﻿namespace S15_Delegate.Service;
+
+public class CalculationService
+{
+    public static double Max(double x, double y)
+    {
+        return (x > y) ? x : y;
+    }
+
+    public static double Sum(double x, double y)
+    {
+        return x + y;
+    }
+    
+    public static double Square(double x)
+    {
+        return x * x;
+    }
+
+    public static void ShowMax(double a, double b)
+    {
+        double max = Max(a, b);
+        Console.WriteLine($"Max: {max}");
+    }
+    
+    public static void ShowSum(double a, double b)
+    {
+        double sum = Sum(a, b);
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
