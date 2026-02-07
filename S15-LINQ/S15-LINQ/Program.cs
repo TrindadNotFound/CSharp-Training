fe09f2c0097dedaaedb47e1b0fb3d5aead627b7b
R. Trindade <75436524+TrindadNotFound@users.noreply.github.com>
2026-02-07 19:53:14 +0000

    Add files via upload

---
+++ S15-LINQ/S15-LINQ/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace S15_LINQ;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Specify data source
+        int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        
+        //Define the query expression
+        var result = numbers
+            .Where(n => n % 2 == 0)
+            .Select(x => x * 10);
+        
+        //Execute query
+        foreach (int i in result)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
