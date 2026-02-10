93f7798d9afd0305c441f1e1b7c7630b325eb299
Ricardo Trindade <ricardo-x-trindade@hotmail.com>
2026-02-10 23:36:15 +0000

    Remove S19-SalesWebMVC

--- S19-SalesWebMVC/S19-SalesWebMVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace S19_SalesWebMVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
